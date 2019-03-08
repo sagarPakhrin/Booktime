5134f368f384a610b27df34643266615ee5efe6c
Sagar Lama <sagarlama686@gmail.com>
2019-03-08 10:39:57 +0545

    Initial setup

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestPage(TestCase):
+
+    """Docstring for TestPage. """
+
+    def test_home_page_workd(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'BookTime')
